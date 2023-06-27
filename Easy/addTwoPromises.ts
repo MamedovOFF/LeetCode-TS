2753dfa22c3ecbd1d478630eb24920df8097eb12
Mdev <mamedov_dev@mail.ru>
2023-06-27 12:15:40 +0200

    addTwoPromises

---
+++ Easy/addTwoPromises.ts
@@ -0,0 +1,7 @@
+async function addTwoPromises(promise1: Promise<number>, promise2: Promise<number>): Promise<number> {
+    const res1 = await promise1
+    const  res2 = await promise2
+    return new Promise((resolve) => resolve(res1 + res2))
+}
+
+addTwoPromises(Promise.resolve(2), Promise.resolve(2)).then((res) => console.log(res))
