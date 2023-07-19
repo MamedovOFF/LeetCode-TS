0a0d57dc232414447e865c154b42b26fdd8372a2
Mdev <mamedov_dev@mail.ru>
2023-07-19 15:13:09 +0200

    eraseOverlapIntervals

---
+++ Medium/eraseOverlapIntervals.ts
@@ -0,0 +1,43 @@
+function eraseOverlapIntervals(intervals: number[][]): number {
+  const copyArr = [...intervals.sort((a, b) => a[1] - b[1])]
+  let prev = -1
+  let res = 0
+  for (let i = 0; i < copyArr.length; i++) {
+    if (copyArr[i][0] > prev) {
+      res++
+    } else {
+      prev = copyArr[i][1]
+    }
+  }
+  return res
+}
+
+// eraseOverlapIntervals([
+//   [1, 2],
+//   [1, 2],
+//   [1, 2],
+// ])
+// console.log(
+//   eraseOverlapIntervals([
+//     [1, 2],
+//     [1, 2],
+//     [1, 2],
+//   ]),
+// )
+
+console.log(
+  eraseOverlapIntervals([
+    [1, 2],
+    [2, 3],
+    [3, 4],
+    [1, 3],
+  ]),
+)
+// console.log(
+//   eraseOverlapIntervals([
+//     [1, 100],
+//     [11, 22],
+//     [1, 11],
+//     [2, 12],
+//   ]),
+// )
